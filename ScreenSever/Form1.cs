4a6f1be882e6e7351c1f34c3eea0190b0aa6d95e
ZamithAhamed <zamith11@gmail.com>
2022-09-17 22:39:28 +0530

    Initial Commit

---
+++ ScreenSever/Form1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ScreenSever
+{
+    public partial class frmScrnSvr : Form
+    {
+        public frmScrnSvr()
+        {
+            InitializeComponent();
+        }
+
+        private void FrmScrnSvr_KeyDown(object sender, KeyEventArgs e)
+        {
+            Close();
+        }
+    }
+}
